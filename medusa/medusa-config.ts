777f7a262c88bfc457a19c1f266663daaf139137
Drew Thompson <drewrox2009@gmail.com>
2025-10-16 08:47:46 -0600

    add seed

--- medusa/medusa-config.ts
+++ medusa/medusa-config.ts
@@ -59,9 +59,11 @@ module.exports = defineConfig({
               region: process.env.S3_REGION,
               bucket: process.env.S3_BUCKET,
               endpoint: process.env.S3_ENDPOINT,
-              additional_client_config: {
-                forcePathStyle: true,
-              },
+              s3_force_path_style: true, // Try this instead of additional_client_config
+              signature_version: "v4",
+              // additional_client_config: {
+              //   forcePathStyle: true,
+              // },
             },
           },
         ],
