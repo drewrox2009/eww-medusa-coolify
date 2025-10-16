3f1ba127c6ab2726f983a7c67232b91fbf1e24b1
drewrox2009 <drewrox2009@gmail.com>
2025-10-16 12:36:03 -0600

    finished

--- front/next.config.js
+++ front/next.config.js
@@ -16,6 +16,18 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "pubchem.ncbi.nlm.nih.gov",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "*.eww-pew.com",
+      },
     ],
   },
   // Enable for Docker deployment
