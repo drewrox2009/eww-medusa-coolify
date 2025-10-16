6cf80845b70f1d452ce23bd99b7a289def0b7146
drewrox2009 <drewrox2009@gmail.com>
2025-10-16 12:13:00 -0600

    finished

--- front/src/lib/utils/format.ts
+++ front/src/lib/utils/format.ts
@@ -3,7 +3,7 @@ export function formatPrice(amount: number, currency: string = "USD"): string {
     style: "currency",
     currency,
     minimumFractionDigits: 2,
-  }).format(amount / 100);
+  }).format(amount);
 }
 
 export function formatPricePerUnit(
